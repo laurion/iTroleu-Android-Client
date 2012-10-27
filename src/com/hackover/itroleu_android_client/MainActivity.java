fa327609a39afc653fc4ef4f337746e1079060cc
Laurentiu Cristian Ion <ionlaurentiucristian@gmail.com>
2012-10-28 01:19:47 +0300

    First Map View

--- src/com/hackover/itroleu_android_client/MainActivity.java
+++ src/com/hackover/itroleu_android_client/MainActivity.java
@@ -1,10 +1,12 @@
 package com.hackover.itroleu_android_client;
 
+import com.google.android.maps.MapActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends MapActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,9 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
     }
 }
