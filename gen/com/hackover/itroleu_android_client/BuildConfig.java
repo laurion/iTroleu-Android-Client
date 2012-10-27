86b9f5700dfa1f8009e0650f79ed1b8302cac9d8
Laurentiu Cristian Ion <ionlaurentiucristian@gmail.com>
2012-10-28 00:31:37 +0300

    initial android app structure

---
+++ gen/com/hackover/itroleu_android_client/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.hackover.itroleu_android_client;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
