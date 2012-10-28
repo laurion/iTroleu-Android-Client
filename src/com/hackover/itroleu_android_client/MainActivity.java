58e7e830ea18896e2a65e4ef26b015e034d19d22
Laurentiu Cristian Ion <ionlaurentiucristian@gmail.com>
2012-10-28 03:28:28 +0300

    first working version

--- src/com/hackover/itroleu_android_client/MainActivity.java
+++ src/com/hackover/itroleu_android_client/MainActivity.java
@@ -1,9 +1,16 @@
 package com.hackover.itroleu_android_client;
 
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.view.Menu;
 
 public class MainActivity extends MapActivity {
@@ -12,6 +19,22 @@ public class MainActivity extends MapActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        
+        mapView.getController().setZoom(12);
+        
+        mapView.setBuiltInZoomControls(true);
+        
+        List<Overlay> mapOverlays = mapView.getOverlays();
+        Drawable drawable = this.getResources().getDrawable(R.drawable.marker);
+        MyItemizedOverlay itemizedoverlay = new MyItemizedOverlay(drawable, this);
+        
+        GeoPoint point = new GeoPoint(44432636, 26103837);
+        OverlayItem overlayitem = new OverlayItem(point, " Bucuresti", "Kilometrul 0");
+        itemizedoverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedoverlay);
+        
+        mapView.getController().setCenter(point);
     }
 
     @Override
